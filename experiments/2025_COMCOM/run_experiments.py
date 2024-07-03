14f52a6933531e3183559dc9e02a6938b8273e2a
sgiammy-rta <sara.giammusso@bancaditalia.it>
2024-07-03 12:08:44 +0000

    comcom: first commit. Just topology for experiment 1

---
+++ experiments/2025_COMCOM/run_experiments.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+from enum import Enum
+from pathlib import Path
+
+from plasma_network_generator.commands.generate_all import (
+    DEFAULT_FRACTION_OF_UNBANKED_RETAIL_USERS,
+)
+from plasma_network_generator.commands.generate_all import (
+    Args as TopologyGeneratorArgs,
+)
+from plasma_network_generator.commands.generate_all import (
+    _execute as topology_generate,
+)
+from plasma_network_generator.core import select_eurosystem_subset
+
+MY_DIR = Path(__file__).resolve().parent
+
+
+class TopologyType(Enum):
+    SH_PCN = "SH_PCN"
+    SF_PCN = "SF_PCN"
+
+
+def setup_topology_directories(topology_type: TopologyType) -> tuple[Path, Path]:
+    """
+    Set up and return the necessary topology directory paths.
+    """
+    cloth_root_dir = MY_DIR.parent.parent
+    topologies_dir = MY_DIR / "topologies" / topology_type.value
+    topologies_dir.mkdir(parents=True, exist_ok=True)
+    return cloth_root_dir, topologies_dir
+
+
+def generate_topologies(
+    topologies_dir: Path,
+    seeds: list[int],
+    capacities: list[float],
+    topology_type: TopologyType,
+) -> None:
+    """
+    Generate topologies for the given seeds and capacities.
+    """
+    for seed in seeds:
+        topologies_seed_dir = topologies_dir / f"seed_{seed}"
+        if topologies_seed_dir.exists():
+            print(f"Skipping {topologies_seed_dir} because it already exists.")
+            continue
+        topgen_args = TopologyGeneratorArgs(
+            model_params_file=MY_DIR / "PCN_model_params.json",
+            nb_partitions=[4],
+            seed=seed,
+            nations=select_eurosystem_subset(["IT", "CY", "FI"]),
+            nb_cb=0 if topology_type == TopologyType.SF_PCN else 3,
+            nb_retail=300000,
+            nb_merchants=3000,
+            nb_intermediaries=30,
+            capacity_fractions=capacities,
+            output_dir=topologies_seed_dir,
+            # Other args
+            version=False,  # Do not print version and exit
+            verbose=False,
+            p_small_merchants=0.4,
+            p_medium_merchants=0.3,
+            p_large_merchants=0.3,
+            fraction_of_unbanked_retail_users=DEFAULT_FRACTION_OF_UNBANKED_RETAIL_USERS,
+            scale_free_2_2=(topology_type == TopologyType.SF_PCN),
+        )
+        topology_generate(topgen_args)
+
+
+def main() -> None:
+    seeds = [
+        7,
+        13,
+        23,
+        42,
+        45,
+    ]
+    capacities = [
+        0.0,
+        0.00001,
+        0.00002,
+        0.00005,
+        0.0001,
+        0.0002,
+        0.0005,
+        0.001,
+        0.0013,
+        0.0015,
+        0.0018,
+        0.002,
+        0.005,
+        0.01,
+        0.02,
+        0.05,
+        0.1,
+        1.0,
+    ]
+    # Topology generation
+    for topology_type in TopologyType:
+        cloth_root_dir, topologies_dir = setup_topology_directories(topology_type)
+        generate_topologies(topologies_dir, seeds, capacities, topology_type)
+
+
+if __name__ == "__main__":
+    main()
