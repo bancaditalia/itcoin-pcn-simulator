16fba57c14878597e3cbf44a812278a235fb75e5
Giuseppe Galano <giuseppe.galano2@bancaditalia.it>
2024-07-08 10:17:00 +0200

    statistics analyzer: blockchain module fixes

--- utilities/statistics_analyzer/blockchain.py
+++ utilities/statistics_analyzer/blockchain.py
@@ -9,6 +9,7 @@ class COLS(str, Enum):
     BLOCK_HEIGHT = "block.height"
     BLOCK_TIME = "block.time"
     SWAP_LATENCY = "swap.latency"
+    SWAP_DISAMBIGUATE_IDX = "swap.disambiguate.idx"
     TX_AMOUNT = "tx.amount"
     TX_RECEIVER = "tx.receiver"
     TX_START_TIME = "tx.start_time"
@@ -28,20 +29,40 @@ def _df_from_blockchain_output(path: str | os.PathLike) -> pd.DataFrame:
 def _add_swap_latencies_to_blockchain_df(blockchain_df: pd.DataFrame) -> pd.DataFrame:
     prepare_htlcs = blockchain_df[
         blockchain_df[COLS.TX_TYPE].str.contains("PREPARE_HTLC")
-    ]
-    claim_htlcs = blockchain_df[blockchain_df[COLS.TX_TYPE].str.contains("CLAIM_HTLC")]
+    ].copy()
+    claim_htlcs = blockchain_df[
+        blockchain_df[COLS.TX_TYPE].str.contains("CLAIM_HTLC")
+    ].copy()
+    prepare_htlcs[COLS.SWAP_DISAMBIGUATE_IDX.value] = (
+        prepare_htlcs.groupby(
+            [COLS.TX_SENDER, COLS.TX_RECEIVER, COLS.TX_AMOUNT]
+        ).cumcount()
+        + 1
+    )
+    claim_htlcs[COLS.SWAP_DISAMBIGUATE_IDX.value] = (
+        claim_htlcs.groupby(
+            [COLS.TX_SENDER, COLS.TX_RECEIVER, COLS.TX_AMOUNT]
+        ).cumcount()
+        + 1
+    )
+
     columns_of_interest = [
         COLS.TX_TYPE,
         COLS.TX_SENDER,
         COLS.TX_RECEIVER,
         COLS.TX_AMOUNT,
         COLS.BLOCK_HEIGHT,
+        COLS.SWAP_DISAMBIGUATE_IDX,
     ]
-
     merged = prepare_htlcs[columns_of_interest + [COLS.TX_START_TIME]].merge(
         claim_htlcs[columns_of_interest + [COLS.BLOCK_TIME]],
         how="left",
-        on=[COLS.TX_SENDER, COLS.TX_RECEIVER, COLS.TX_AMOUNT],
+        on=[
+            COLS.TX_SENDER,
+            COLS.TX_RECEIVER,
+            COLS.TX_AMOUNT,
+            COLS.SWAP_DISAMBIGUATE_IDX,
+        ],
         suffixes=["_prepare", "_claim"],
         validate="one_to_one",
     )
@@ -49,13 +70,15 @@ def _add_swap_latencies_to_blockchain_df(blockchain_df: pd.DataFrame) -> pd.Data
     merged = merged.dropna(
         how="any", subset=[COLS.BLOCK_HEIGHT + "_claim", COLS.BLOCK_TIME]
     )
-    merged[COLS.SWAP_LATENCY] = merged[COLS.BLOCK_TIME] - merged[COLS.TX_START_TIME]
+    merged[COLS.SWAP_LATENCY.value] = (
+        merged[COLS.BLOCK_TIME] - merged[COLS.TX_START_TIME]
+    ).apply(lambda x: round(x, 2))
     return merged
 
 
 def get_swap_latencies_from_blockchain_output(
     blockchain_output: str | os.PathLike,
-) -> list:
+) -> pd.core.series.Series:
     blockchain_df = _df_from_blockchain_output(blockchain_output)
     blockchain_df_with_latencies = _add_swap_latencies_to_blockchain_df(blockchain_df)
-    return blockchain_df_with_latencies[COLS.SWAP_LATENCY].to_list()
+    return blockchain_df_with_latencies[COLS.SWAP_LATENCY]
