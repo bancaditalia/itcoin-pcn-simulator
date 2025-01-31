e3a6bce24812d2b309b47cbfc51b052a396b2452
muxator <antonio.muci@bancaditalia.it>
2025-01-31 14:04:43 +0100

    notebook: support optionally loading compressed files
    With this change, the notebook works in both cases:
    - result files exist and are in uncompressed .csv format (like before
    - result files exist and are in compressed .csv.gz format (this reduces space
      occupation to ~20% of the original size)

--- experiments/2024_COMCOM/utils.py
+++ experiments/2024_COMCOM/utils.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
 import pathlib
+from typing import TYPE_CHECKING
 
 import IPython
+
+if TYPE_CHECKING:
+    import os
 
 
 def get_notebook_path_or_cwd() -> pathlib.Path:
@@ -22,3 +26,19 @@ def get_notebook_path_or_cwd() -> pathlib.Path:
     if "__session__" in user_ns:
         return pathlib.Path(user_ns["__session__"]).resolve().parent
     return pathlib.Path.cwd()
+
+
+def add_gz_extension_if_exists(filepath: str | os.PathLike) -> pathlib.Path:
+    """Check if filepath.gz exists and return that. Otherwise return filepath.
+
+    If none of filepath.gz and filepath exist, throws a FileNotFoundError.
+    The result is converted to an absolute pathlib.Path object.
+    """
+    abs_path = pathlib.Path(filepath).resolve()
+    gz_path = abs_path.parent / (abs_path.name + ".gz")
+    if gz_path.is_file():
+        return gz_path
+    if abs_path.is_file():
+        return abs_path
+    msg = f"Could not find {abs_path} or {gz_path}"
+    raise FileNotFoundError(msg)
