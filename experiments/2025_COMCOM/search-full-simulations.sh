a592cf53b51050a9c1b5f138fa20259c4f3caa72
muxator <antonio.muci@bancaditalia.it>
2025-01-31 14:49:16 +0100

    utils: implemented search_full_simulations(), which translates search-for-full.sh in python

--- experiments/2025_COMCOM/search-full-simulations.sh
+++ experiments/2025_COMCOM/search-full-simulations.sh
@@ -6,4 +6,4 @@ printf "These are the directories that have to be used instead of -FULL:\n\n"
 printf "SF: %s\n" $(rg --files-with-matches --glob "simulation_log.txt" -- "--waterfall=1.*--reverse-waterfall=1.*--submarine-swaps=1" results/exp-1/SF_PCN/)
 echo
 printf "SH: %s\n" $(rg --files-with-matches --glob "simulation_log.txt" -- "--waterfall=1.*--reverse-waterfall=1.*--submarine-swaps=1" results/exp-1/SH_PCN/)
-printf "\nTODO: reimplement this in python!\n"
+printf "\nThis search algorithm is also implemented in utils.py:search_full_simulations\n"
