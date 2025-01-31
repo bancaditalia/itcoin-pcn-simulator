1667f6a65d1facdf8ff2907cba4e73b6eb5631ab
muxator <antonio.muci@bancaditalia.it>
2025-01-31 14:04:43 +0100

    notebook: support optionally loading compressed files
    With this change, the notebook works in both cases:
    - result files exist and are in uncompressed .csv format (like before
    - result files exist and are in compressed .csv.gz format (this reduces space
      occupation to ~20% of the original size)

---
+++ experiments/2025_COMCOM/results/compress.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+
+CSV_FULLPATHS=( $(find . -type f -name "*.csv") )
+
+for CSV_FULLPATH in "${CSV_FULLPATHS[@]}"; do
+	CSV_FULLPATH=$(realpath "${CSV_FULLPATH}")
+	DIRECTORY=$(dirname "${CSV_FULLPATH}")
+	FILENAME=$(basename "${CSV_FULLPATH}")
+	printf "DIR: %s, FNAME: %s, FULL_PATH: %s\n" "${DIRECTORY}" "${FILENAME}" "${CSV_FULLPATH}"
+	pushd "${DIRECTORY}"
+	gzip -8 "${FILENAME}"
+	popd
+done
