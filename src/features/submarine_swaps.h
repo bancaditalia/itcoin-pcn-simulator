019f9df358bdad104b658f224da7fe6bdedb1ed5
muxator <antonio.muci@bancaditalia.it>
2025-02-10 17:45:28 +0100

    simulator: remove "typedef" from "struct submarine_swap" in submarine_swaps.h

--- src/features/submarine_swaps.h
+++ src/features/submarine_swaps.h
@@ -14,14 +14,14 @@ typedef enum submarine_swap_state {
   L1_CLAIMED
 } submarine_swap_state;
 
-typedef struct submarine_swap {
+struct submarine_swap {
   long submarine_sender;
   long submarine_receiver;
   long amount;
   long trigger_payment_id;
   double start_time;
   submarine_swap_state state;
-} submarine_swap;
+};
 
 // Event handling functions
 void submarine_swaps_on_forward_payment(tw_lp *lp, struct message *in_msg);
@@ -38,8 +38,8 @@ void submarine_swaps_on_receive_success(tw_lp *lp, struct payment* payment);
 void submarine_swaps_on_receive_success_rev(tw_lp *lp, struct payment* payment);
 
 // Serialization and deserialization functions
-void serialize_submarine_swap(submarine_swap* tx, char* serialized);
-submarine_swap* deserialize_submarine_swap(const char* serialized);
+void serialize_submarine_swap(struct submarine_swap* tx, char* serialized);
+struct submarine_swap* deserialize_submarine_swap(const char* serialized);
 
 #endif
 
